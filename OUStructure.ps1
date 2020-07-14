d66473f141e768c26293748df4385ba63718b4bd
edwin <1850115698@qq.com>
2020-07-14 21:54:04 +0800

    upload

---
+++ OUStructure.ps1
@@ -0,0 +1,16 @@
+﻿Get-Content -Path C:\Users\Administrator\Desktop\OUStructure.csv | ForEach-Object{
+    $DomainName = "DC=2kgames,DC=t2,DC=corp"
+    $OrgUnitPath = ""
+    $OrgUnits = (Split-Path $_ -Parent).Split("\")
+    [array]::Reverse($OrgUnits)
+    $OrgUnits | ForEach-Object{
+    if($_.Length -eq 0){
+    return
+    }
+    $OrgUnitPath = $OrgUnitPath + "OU=" + $_ +","
+    }
+    $OrgUnitPath +=$DomainName
+    $NewOrgUnitName = Split-Path $_ -Leaf
+    New-ADOrganizationalUnit -Name "$NewOrgUnitName" -Path "$OrgUnitPath" -ProtectedFromAccidentalDeletion $false
+
+}
